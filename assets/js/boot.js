b6edf84b64f9f59ad6c9b0afb90ea9d03f0b677d
Tristan Caron <tcaron@umanit.fr>
2016-11-10 17:02:32 +0100

    ajout bootjs

---
+++ assets/js/boot.js
@@ -0,0 +1,24 @@
+/*****************************************************************************
+ * Omaha - Point d'entrée
+ * Chargement des modules js
+ * @author tcaron <tristan.caron@free.fr>
+ *****************************************************************************/
+
+;(function (jq, container) {
+  'use strict';
+  jq(function ($) {
+
+    var modules = $('html').data('modules').split(/\s+/);
+
+    for (var i = 0; i < modules.length; i++) {
+      if (container[modules[i]] === undefined) {
+        continue;
+      }
+
+      container[modules[i]]();
+    }
+  });
+})(
+  window.jQuery,
+  window
+);
