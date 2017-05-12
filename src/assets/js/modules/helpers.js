de3ec5b4f3daf2656d4ea29250abf51e3d0a4600
Hugo Dessomme <hdessomme@umanit.fr>
2017-05-12 15:49:22 +0200

    Ré-organisation de l'architecture

---
+++ src/assets/js/modules/helpers.js
@@ -0,0 +1,36 @@
+/*****************************************************************************
+ * Module Javascript chargeant les helpers
+ * @author tcaron <tristan.caron@free.fr>
+ *****************************************************************************/
+
+;(function(exports, $) {
+    exports.helpers= function() {
+
+        // Helpers responsive
+
+        window.BREAKPOINTS = {
+            xs: 768,
+            sm: 992,
+            md: 1200
+        };
+
+        function is(size){
+            return $(document).width() <= size;
+        }
+
+        window.isMobile = function(){
+            return is(window.BREAKPOINTS.xs);
+        };
+
+        window.isTablet = function(){
+            return is(window.BREAKPOINTS.sm);
+        };
+
+        window.isDesktop = function(){
+            return is(window.BREAKPOINTS.md);
+        };
+    }
+})(
+  window,
+  window.jQuery
+);
